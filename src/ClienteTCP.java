3e166abaf6d45483ea83a0baea22ab931597e604
Hulk-lian <juliantrodom@gmail.com>
2016-12-01 19:44:16 +0100

    que se me habia olvidado guardarlo

--- src/ClienteTCP.java
+++ src/ClienteTCP.java
@@ -1,11 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ClienteTCP {
-
-	/**
-	 * @param args
-	 */
+	
+	private String HOST="192.168.3.57";
+	private int PORT=5000;
+	private Socket socket;
+	
+	public ClienteTCP() {
+		try {
+			socket= new Socket(HOST,PORT);
+			//leer mensaje de exito de conexion
+			InputStreamReader is= new InputStreamReader(socket.getInputStream());
+			BufferedReader br= new BufferedReader(is);
+			
+			System.out.println(br.readLine());
+			
+			socket.close();
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new ClienteTCP();
 
 	}
 
